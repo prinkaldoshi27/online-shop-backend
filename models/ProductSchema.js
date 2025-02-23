fd92776fdbed1f6b21e1452a01c77b6ea321f138
Prinkal <prinkaldoshi@gmail.com>
2025-02-24 00:19:21 +0530

    comments added.

--- models/ProductSchema.js
+++ models/ProductSchema.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
+const CommentSchema = new mongoose.Schema({
+    name: { type: String, required: true },  
+    rating: { type: Number, required: true, min: 1, max: 5, default: 3 }, 
+    comment: { type: String, required: true },  
+    createdAt: { type: Date, default: Date.now }  
+});
+
 const ProductSchema = new mongoose.Schema({
-    id: {type : Number, required: true},
+    id: { type: Number, required: true },
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
     category: { type: String },
     quantity: { type: Number },
     inventoryStatus: { type: String },
-    rating: { type: Number ,
-        default: 3
-    },
-    image: {
-        type: String,
-        required: true,
-    }
+    rating: { type: Number, default: 3 },
+    image: { type: String, required: true },
+    comments: { type: [CommentSchema], default: [] } 
 }, { versionKey: false });
 
 const Product = mongoose.model("products", ProductSchema);
